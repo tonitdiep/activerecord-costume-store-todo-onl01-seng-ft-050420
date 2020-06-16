77a6e7065aee526fb511f08eda101d21cd285d82
Toni  Diep <adiep123@gmail.com>
2020-06-16 23:37:20 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,3 +7,22 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+
+
+class CreateCostumes < ActiveRecord::Migration
+  
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :price
+      t.integer :size
+      t.string :image_url
+      t.integer :create_at 
+      t.integer :updated_at
+      
+      t.timestamps 
+    end
+  end
+end
+  
+ 
